be0f462a273d0798e458649161c7408092be07d0
Murilo Klein <0100733@notredame.org.br>
2024-09-19 15:27:23 -0300

    Adicionado relação das entidades de filosofo e cientista com o entidade cidade

--- src/entities/filosofo.ts
+++ src/entities/filosofo.ts
@@ -1,4 +1,5 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import Cidade from "./cidade";
 
 @Entity()
 export class Filosofo {
@@ -14,11 +15,15 @@ export class Filosofo {
   @Column()
   deathDay?: Date;
 
-  constructor(id?: number, name?: string, birthDay?: Date, deathDay?: Date) {
+  @ManyToOne(() => Cidade, (cidade: { id: number; }) => cidade.id)
+  birhCity?: Cidade;
+
+  constructor(id?: number, name?: string, birthDay?: Date, deathDay?: Date, birthCity?: Cidade) {
     this.id = id;
     this.name = name;
     this.birthDay = birthDay;
     this.deathDay = deathDay;
+    this.birhCity = birthCity;
   }
 }
 
